250c5203a3eec77826d19cd7052cced3804ac5a3
Zhaoen Zheng <zhaoen1023@gmail.com>
2025-08-12 14:49:15 +0200

    home page demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
+
 function App() {
-
-
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-      <Route />
-      <Route />
-
+        111
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      </BrowserRouter>
-    </>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+    // 确保 Hooks 只在函数组件顶层调用
+    // const [state, setState] = useState() // 如果有的话
+    
+    return (
+        <div>
+            Home
+        </div>
+    )
+};
