78f486a4a0dfc10d7042e5c7429fb4b7286525ab
Zhaoen Zheng <zhaoen1023@gmail.com>
2025-08-12 11:54:55 +0200

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+      <Route />
+      <Route />
+
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
